0b67e5c924ceea52e0dd0215cc5f0e33d4c46dc5
Efrain Basualdo <efrainbasualdo20@gmail.com>
2025-06-25 21:08:56 +0200

    Variables sensibles(clave JWT, access_token de JWT y refresh_token de JWT) protegidas(.env)

--- myweb/env.py
+++ myweb/env.py
@@ -6,3 +6,6 @@ load_dotenv(find_dotenv())
 DEBUG = os.getenv("DEBUG") == "True"
 SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
+JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY") 
+JWT_ACCESS_TOKEN_EXPIRATION = int(os.getenv("JWT_ACCESS_TOKEN_EXPIRATION"))  
+JWT_REFRESH_TOKEN_EXPIRATION = int(os.getenv("JWT_REFRESH_TOKEN_EXPIRATION")) 
