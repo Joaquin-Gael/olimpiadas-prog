62c1c2ee3c7876862b21f4d56c2fc4ce7064c71d
git-Joaquin-Gael <guzmanantonio867@gmail.com>
2025-06-28 03:07:16 -0300

    docs(mensages para explicar instrucciones): dev

--- myweb/setup-dev.ps1
+++ myweb/setup-dev.ps1
@@ -1,1 +1,13 @@
+$msg = "para produccion ejecutar uv remove psycopg y psycopg2"
+$instruction = "al agregar dependencias espesificas para el desarrollo espesificar arriba"
+
+$width = $Host.UI.RawUI.WindowSize.Width
+
+$pad = $width - $msg.Length
+
+$left = "-" * [math]::Floor($pad/2)
+$right = "-" * [math]::Ceiling($pad/2)
+
+Write-Host "$left$msg$right"
+Write-Host "$left$instruction$right"
 uvicorn.exe myweb.asgi:application --host 127.0.0.1 --port 8080 --reload
